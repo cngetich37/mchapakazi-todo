6640929a792dc9114d2b74dfee104e401ff8f575
cngetich37 <cngetich37@gmail.com>
2023-09-15 14:42:49 +0300

    updated the mchapakazi todo

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const errorHandler = require("./middleware/errorhandler");
 const connectDb = require("./connectdb/dbConnection");
 const dotenv = require("dotenv").config();
-
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 
 connectDb();
+app.use(cors({origin:'http://localhost:5173'}))
 app.use(express.json());
 app.use("/api/todos", require("./routes/todoRoute"));
 app.use(errorHandler)
